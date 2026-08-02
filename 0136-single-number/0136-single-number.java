71f30f8a1cb59ef835e386d1a45af401ccc5c692
manasifalake07-ctrl <manasifalake07@gmail.com>
2026-08-02 15:01:06 +0530

    Time: 531 ms (8.24%), Space: 46.9 MB (46.67%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,45 @@
+// class Solution {
+//     public int singleNumber(int[] nums) {
+//         for(int i = 0 ; i < nums.length ; i++) {
+//             int count = 1;
+//             for(int j = 0 ; j < nums.length ; j++) {
+//                 if(i == j) {
+//                     continue;
+//                 }
+//                 if(nums[i] == nums[j]) {
+//                     count++;
+//                 }
+//             }
+//             if(count == 1) {
+//                 return(nums[i]);
+//             }
+//         }
+//         return(-1);
+//     }
+// }
+
+
+class Solution {
+    public int singleNumber(int[] nums) {
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int count = 0;
+
+            for (int j = 0; j < nums.length; j++) {
+
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+
+            }
+
+            if (count == 1) {
+                return nums[i];
+            }
+
+        }
+
+        return -1;
+    }
+}
