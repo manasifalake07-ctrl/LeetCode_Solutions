7fe6b52b015485bfab030f5148ec84c6ef2d2a55
manasifalake07-ctrl <manasifalake07@gmail.com>
2026-08-02 15:02:24 +0530

    Time: 1 ms (99.88%), Space: 46.9 MB (98.33%) - LeetHub

---
+++ 3033-modify-the-matrix/3033-modify-the-matrix.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[][] modifiedMatrix(int[][] matrix) {
+        for(int j = 0 ; j < matrix[0].length ; j++) {
+            int max = matrix[0][j];
+            for(int i = 0 ; i < matrix.length ; i++) {
+                if(matrix[i][j] > max) {
+                    max = matrix[i][j];
+                }
+            }
+            for(int i = 0 ; i < matrix.length ; i++) {
+                if(matrix[i][j] == -1) {
+                    matrix[i][j] = max;
+                }                
+            }
+        }
+        return(matrix);
+    }
+}
