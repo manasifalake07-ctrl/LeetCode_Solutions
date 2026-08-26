103a8b27bf46cf5afefafaeb02b6e4ac537e81d2
Manasi Falake <manasifalake07@gmail.com>
2026-08-26 21:05:30 +0530

    Time: 1 ms (99.94%), Space: 46.8 MB (82.48%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -21,25 +21,22 @@
 
 class Solution {
     public int singleNumber(int[] nums) {
-
-        for (int i = 0; i < nums.length; i++) {
-
-            int count = 0;
-
-            for (int j = 0; j < nums.length; j++) {
-
-                if (nums[i] == nums[j]) {
-                    count++;
-                }
-
-            }
-
-            if (count == 1) {
-                return nums[i];
-            }
-
+        // for (int i = 0; i < nums.length; i++) {
+        //     int count = 0;
+        //     for (int j = 0; j < nums.length; j++) {
+        //         if (nums[i] == nums[j]) {
+        //             count++;
+        //         }
+        //     }
+        //     if (count == 1) {
+        //         return(nums[i]);
+        //     }
+        // }
+        // return(-1);
+        int x = 0;
+        for(int v : nums) {
+            x^=v;
         }
-
-        return -1;
+        return(x);
     }
 }
